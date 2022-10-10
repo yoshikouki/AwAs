a43b442736459cddd8f4393e6b9c21ce0aedf81c
yoshikouki <yoshikouki@gmail.com>
2022-10-10 23:49:31 +0900

    Separate page component into layouts and components

---
+++ frontend/layouts/default.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { ReactNode } from 'react';
+
+interface Props {
+  title?: string;
+  children?: ReactNode;
+}
+
+const DefaultLayout = ({ title, children }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{title}Fundamental Charts</title>
+        <meta name="description" content="Grate charts for fundamental analysis." />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <main>
+        {children}
+      </main>
+
+      <footer>
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fundamental Charts
+        </a>
+      </footer>
+    </>
+  )
+}
+
+export default DefaultLayout
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,34 +1,15 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Home from '../components/Home';
+import DefaultLayout from '../layouts/default';
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Fundamental Charts</title>
-        <meta name="description" content="Grate charts for fundamental analysis." />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-
-      <main>
-      </main>
-
-      <footer>
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fundamental Charts
-        </a>
-      </footer>
+      <DefaultLayout>
+        <Home />
+      </DefaultLayout>
     </>
   )
 }
 
-export default Home
+export default HomePage
