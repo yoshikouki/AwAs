908bd1afb11b3159b760fb4638f3e74dd92e40d8
yoshikouki <yoshikouki@gmail.com>
2022-10-17 00:15:29 +0900

    モードの設定はクライアントに保存しておく

--- frontend/components/ThemeContextProvider.tsx
+++ frontend/components/ThemeContextProvider.tsx
@@ -8,17 +8,27 @@ interface Props {
 
 const ThemeContextProvider = ({ children }: Props) => {
   const [theme, setTheme] = useState<ThemeContextType["theme"]>("light");
-  const changeTheme = (theme: ThemeContextType["theme"]) => setTheme(theme);
-
-  useEffect(() => {
+  const changeTheme = (theme: ThemeContextType["theme"]) => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
       document.documentElement.dataset.theme = "halloween"
+      localStorage.theme = "dark"
     } else {
       document.documentElement.classList.remove("dark");
       document.documentElement.dataset.theme = "cupcake"
+      localStorage.theme = "light"
     };
-  }, [theme]);
+    setTheme(theme)
+  };
+
+  useEffect(() => {
+    const hadPreferenceForDark = (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    if (localStorage.theme === "dark" || hadPreferenceForDark) {
+      changeTheme("dark")
+    } else {
+      changeTheme("light")
+    }
+  }, []);
   return (
     <ThemeContext.Provider
       value={{
