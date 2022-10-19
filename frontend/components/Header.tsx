f8c2786c14a8628ffe78b37b1b9bf056214bf1cc
yoshikouki <yoshikouki@gmail.com>
2022-10-19 22:31:45 +0900

    Simple bottom nav

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import useTheme from "../hooks/theme";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 const Header = () => {
   const themeCtx = useTheme();
@@ -13,13 +14,19 @@ const Header = () => {
         <div className="flex-1">
           <Link href="/">
             <a>
-              <h1>FunCh</h1>
+              <h1 className="text-2xl font-bold text-primary">
+                FunCh
+              </h1>
             </a>
           </Link>
         </div>
         <div className="flex-none">
           <button className="btn btn-square btn-ghost" onClick={toggleTheme}>
-            {themeCtx.theme === "light" ? "🌞" : "🌙"}
+            {themeCtx.theme === "light" ? (
+              <FaSun className="h-6 w-6" />
+            ) : (
+              <FaMoon className="h-6 w-6" />
+            )}
           </button>
         </div>
       </header>
