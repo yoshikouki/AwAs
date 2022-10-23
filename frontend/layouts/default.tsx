6d2c53b30f6f7a55d9e52309e89e120db484a57d
yoshikouki <yoshikouki@gmail.com>
2022-10-23 21:14:14 +0900

    transfer head tag responsibilities

--- frontend/layouts/default.tsx
+++ frontend/layouts/default.tsx
@@ -14,31 +14,6 @@ const DefaultLayout = ({ title, children }: Props) => {
     <>
       <Head>
         <title>{(title ? `${title} |` : "") + "Fundamental Charts"}</title>
-        <meta
-          name="description"
-          content="Grate charts for fundamental analysis."
-        />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <HeaderNavigation />
