2f8f2e1554449e5cb218f971fa7b211197ec8431
yoshikouki <yoshikouki@gmail.com>
2022-10-23 22:23:52 +0900

    Viewport meta tags should be used in _app.tsx
    https://nextjs.org/docs/messages/no-document-viewport-meta

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -3,18 +3,25 @@ import type { AppProps } from 'next/app'
 import ThemeContextProvider from '../components/ThemeContextProvider'
 import { Auth0Provider } from '@auth0/auth0-react';
 import { auth0 } from '../config/auth0';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeContextProvider>
-      <Auth0Provider
-        domain={auth0.issuerBaseUrl}
-        clientId={auth0.clientId}
-        redirectUri={auth0.baseUrl}
-      >
-        <Component {...pageProps} />
-      </Auth0Provider>
-    </ThemeContextProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <ThemeContextProvider>
+        <Auth0Provider
+          domain={auth0.issuerBaseUrl}
+          clientId={auth0.clientId}
+          redirectUri={auth0.baseUrl}
+        >
+          <Component {...pageProps} />
+        </Auth0Provider>
+      </ThemeContextProvider>
+    </>
   )
 }
 
