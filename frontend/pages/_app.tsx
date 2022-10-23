f4cd6ff6a4c7dbad9a5502b4080d6ad9461d1718
yoshikouki <yoshikouki@gmail.com>
2022-10-23 10:18:42 +0900

    Install and  Setup Auth0

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ThemeContextProvider from '../components/ThemeContextProvider'
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeContextProvider>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </ThemeContextProvider>
   )
 }
