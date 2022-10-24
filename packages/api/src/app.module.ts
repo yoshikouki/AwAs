ebcc32d174ecf26ae47401c7e6e62a68d3f98c36
yoshikouki <yoshikouki@gmail.com>
2022-10-24 21:13:28 +0900

    Create settings module

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, SettingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
