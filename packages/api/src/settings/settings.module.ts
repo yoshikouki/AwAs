1010501d410dd04b3434c93a6a4eb3363da93073
yoshikouki <yoshikouki@gmail.com>
2022-10-27 17:18:09 +0900

    Replace API with Vite

--- packages/api/src/settings/settings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SettingsController } from './settings.controller';
-import { SettingsService } from './settings.service';
-
-@Module({
-  controllers: [SettingsController],
-  providers: [SettingsService]
-})
-export class SettingsModule {}
