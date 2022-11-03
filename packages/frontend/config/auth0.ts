aad9ab0a6fc937dc527ba76511caec3abac43175
yoshikouki <yoshikouki@gmail.com>
2022-11-03 19:01:21 +0900

    Exposing Env to the Browser

--- packages/frontend/config/auth0.ts
+++ packages/frontend/config/auth0.ts
@@ -1,8 +1,8 @@
 export const auth0 = {
   secret: process.env.AUTH0_SECRET,
-  baseUrl: process.env.PUBLIC_AUTH0_BASE_URL || "http://localhost:8888",
-  issuerBaseUrl: process.env.PUBLIC_AUTH0_ISSUER_BASE_URL || "https://awesome-assets.jp.auth0.com",
-  audienceBaseUrl: process.env.PUBLIC_AUTH0_AUDIENCE_BASE_URL || "http://localhost:8889",
-  clientId: process.env.PUBLIC_AUTH0_CLIENT_ID || "M2oLYOypjBA743vQdE1uhQhFNPbBXsze",
+  baseUrl: process.env.NEXT_PUBLIC_AUTH0_BASE_URL,
+  issuerBaseUrl: process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL,
+  audienceBaseUrl: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE_BASE_URL,
+  clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-}
+};
