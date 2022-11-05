0580c72355c746da48ef806d9a7ad0c4027cf845
yoshikouki <yoshikouki@gmail.com>
2022-11-05 21:30:37 +0900

    Add extra space in footer

--- packages/frontend/components/Footer.tsx
+++ packages/frontend/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer items-center px-4 mt-12">
+    <footer className="footer items-center px-4 mt-12 pb-40">
       <div className="items-center grid-flow-col">
         <p className="text-xs text-slate-500 dark:border-slate-200/5">
           © 2022 AwAs
