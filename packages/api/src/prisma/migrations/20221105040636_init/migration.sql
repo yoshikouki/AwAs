5b45182a37dd466afbf3918a242df393677caa79
yoshikouki <yoshikouki@gmail.com>
2022-11-05 13:08:47 +0900

    Initial prisma migrate and install prisma client

---
+++ packages/api/src/prisma/migrations/20221105040636_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "uid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_uid_key" ON "users"("uid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
