74b58c7e49f2c660e984b5af8b610f5825976346
yoshikouki <yoshikouki@gmail.com>
2022-11-08 23:42:14 +0900

    Build API validation with express-validator

--- packages/api/src/routes/settings.ts
+++ packages/api/src/routes/settings.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 import { validateAccessToken } from "../middleware/auth0.middleware";
 import { SettingsService } from "../services/settings.service";
 
@@ -13,17 +14,32 @@ router.get("/settings", validateAccessToken, async (req, res, _) => {
   res.status(200).json(settings);
 });
 
-router.patch("/settings/profile", validateAccessToken, async (req, res, _) => {
-  const uid = req.auth?.payload.sub;
-  if (!uid) return res.status(401).json({ message: "Unauthorized" });
+router.patch(
+  "/settings/profile",
+  validateAccessToken,
+  body("email").isEmail().normalizeEmail(),
+  body("name").isLength({ min: 2, max: 15 }).trim().escape(),
+  async (req, res, _) => {
+    const uid = req.auth?.payload.sub;
+    if (!uid) return res.status(401).json({ message: "Unauthorized" });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  const settingsService = new SettingsService();
-  const { profile, error } = await settingsService.updateOnUser({ uid, name: req.body.name, email: req.body.email });
-  if (error) {
-    res.status(400).json({ message: "Bad Request", error });
-  } else {
-    res.status(200).json(profile);
+
+    const settingsService = new SettingsService();
+    const { profile, error } = await settingsService.updateOnUser({
+      uid,
+      name: req.body.name,
+      email: req.body.email,
+    });
+    if (error) {
+      res.status(400).json({ message: "Bad Request", error });
+    } else {
+      res.status(200).json(profile);
+    }
   }
-})
+);
 
 export default router;
