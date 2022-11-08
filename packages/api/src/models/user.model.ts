2e207241cd021464250fc925ab4b63bc1901f9bf
yoshikouki <yoshikouki@gmail.com>
2022-11-08 09:35:10 +0900

    Fix prisma connect

--- packages/api/src/models/user.model.ts
+++ packages/api/src/models/user.model.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { prisma } from "../prisma/client";
 
 export class UserModel {
   readonly prisma: PrismaClient;
 
   constructor(props?: Partial<UserModel>) {
-    this.prisma = props?.prisma || new PrismaClient();
+    this.prisma = props?.prisma || prisma;
   }
 
   findOneUserByUid({ uid }: { uid: string }) {
