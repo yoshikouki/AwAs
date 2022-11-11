45e0cded6ffeafe5c84c7c8cb1f532242590087d
yoshikouki <yoshikouki@gmail.com>
2022-11-11 09:34:59 +0900

    Fix user test

--- packages/api/src/models/user.model.test.ts
+++ packages/api/src/models/user.model.test.ts
@@ -6,10 +6,11 @@ const userModel = new UserModel();
 describe("UserModel", () => {
   describe("#findOrCreateByUid", () => {
     test("should create new user ", async () => {
+      expect(await prisma.user.count()).toBe(0);
       const user = await userModel.findOrCreateByUid({
         uid: "test_uid",
       });
-      expect(user).toHaveProperty(["uid", "test_uid"]);
+      expect(user).toHaveProperty("uid", "test_uid");
       expect(await prisma.user.count()).toBe(1);
     });
   });
