7d52b0f55d950cdb72b9a82686630c2d5797cce4
yoshikouki <yoshikouki@gmail.com>
2022-11-12 23:57:56 +0900

    Output query result of prisma

--- packages/api/src/prisma/client.ts
+++ packages/api/src/prisma/client.ts
@@ -1,4 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+  const result = await next(params);
+  const after = Date.now();
+  if ((process.env.NODE_ENV === "development" && params.model)) {
+  // if ((process.env.NODE_ENV !== "production" && params.model)) {
+    console.log(
+      `${params.model} #${params.action} - ${after - before}ms`
+    );
+  }
+  return result;
+});
+
 export default prisma
