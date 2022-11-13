814b760eb5135473f19343d6d2276c67a8cfd96c
yoshikouki <yoshikouki@gmail.com>
2022-11-13 15:25:27 +0900

    To prevent verbose creation queries from executing factory

--- packages/api/src/prisma/factories/holding-asset.factory.ts
+++ packages/api/src/prisma/factories/holding-asset.factory.ts
@@ -6,20 +6,17 @@ import { UserFactory } from "./user.factory";
 
 type CreateInputType = Prisma.HoldingAssetUncheckedCreateInput;
 const modelName = "holdingAsset";
-const getDefaultAttributes = async () => ({
-  userId: (await UserFactory.create()).id,
-  stockId: (await StockFactory.create()).id,
-  balance: faker.datatype.number(),
-  averageTradedPrice: faker.datatype.float({ min: 1 }),
+const getAttributes = async (attrs?: Partial<CreateInputType>) => ({
+  userId: attrs?.userId || (await UserFactory.create()).id,
+  stockId: attrs?.stockId || (await StockFactory.create()).id,
+  balance: attrs?.balance || faker.datatype.number(),
+  averageTradedPrice: attrs?.averageTradedPrice || faker.datatype.float({ min: 1 }),
 });
 
 export const HoldingAssetFactory = {
   create: async (attrs?: Partial<CreateInputType>) => {
     return await prisma[modelName].create({
-      data: {
-        ...(await getDefaultAttributes()),
-        ...attrs,
-      },
+      data: await getAttributes(attrs),
     });
   },
 };
