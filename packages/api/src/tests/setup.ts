0c2c09d15f3af331fed6daca578ed479382bc735
yoshikouki <yoshikouki@gmail.com>
2022-11-13 11:36:26 +0900

    Improve Jest's verbose logs

--- packages/api/src/tests/setup.ts
+++ packages/api/src/tests/setup.ts
@@ -1,6 +1,9 @@
+import console from "console";
 import prisma from "../prisma/client";
 import { cleanupDatabase, resetDatabase } from "./helper";
 
 beforeAll(async () => await resetDatabase());
 beforeEach(async () => await cleanupDatabase());
 afterAll(async () => await prisma.$disconnect());
+
+global.console = console;
