2331f7117fa9acb8ec43f9d985878bac017a873c
yoshikouki <yoshikouki@gmail.com>
2022-11-13 11:18:13 +0900

    Update prisma log to Event-based logging

--- packages/api/src/prisma/client.ts
+++ packages/api/src/prisma/client.ts
@@ -1,18 +1,33 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { logger } from "../utils/logger";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      level: "query",
+      emit: "event",
+    },
+    {
+      level: "info",
+      emit: "event",
+    },
+    {
+      level: "warn",
+      emit: "stdout",
+    },
+    {
+      level: "error",
+      emit: "stdout",
+    },
+  ],
+});
 
-prisma.$use(async (params, next) => {
-  const before = Date.now();
-  const result = await next(params);
-  const after = Date.now();
-  if ((process.env.NODE_ENV === "development" && params.model)) {
-  // if ((process.env.NODE_ENV !== "production" && params.model)) {
-    console.log(
-      `${params.model} #${params.action} - ${after - before}ms`
-    );
+prisma.$on("query", (e: Prisma.QueryEvent) => {
+  if ((process.env.NODE_ENV === "development")) {
+  // if ((process.env.NODE_ENV !== "production")) {
+    logger.log(`[${e.timestamp}] prisma:query [${e.duration}ms] params: ${e.params}`);
+    logger.sql(e.query);
   }
-  return result;
 });
 
 export default prisma
