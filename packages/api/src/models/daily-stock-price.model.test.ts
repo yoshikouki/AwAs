9d013c2f59c7bfb6dd2a2a301796c9f8ef6eb4da
yoshikouki <yoshikouki@gmail.com>
2022-11-23 17:12:52 +0900

    Create scaffold to DailyStockPriceModel #findOrCreateAll and test

---
+++ packages/api/src/models/daily-stock-price.model.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "vitest";
+import { FmpApi } from "../lib/fmp-api";
+import prisma from "../prisma/client";
+import { StockFactory } from "../prisma/factories/stock.factory";
+import { DailyStockPriceModel } from "./daily-stock-price.model";
+
+// const fmpApi = new FmpApi({ apiKey: "testApiKey" });
+const fmpApi = new FmpApi();
+const dailyStockPriceModel = new DailyStockPriceModel({ fmpApi });
+
+describe("DailyStockPriceModel", () => {
+  describe("#findOrCreateAll", () => {
+    describe("when the daily stock prices doesn't exist", () => {
+      test("should create a new daily stock price", async () => {
+        const stock = await StockFactory.create({ symbol: "AAPL" });
+        expect(await prisma.dailyStockPrice.count()).toEqual(0);
+        await dailyStockPriceModel.findOrCreateAll({
+          stocks: [stock],
+        });
+        expect(await prisma.dailyStockPrice.count()).toEqual(1);
+      });
+    });;
+  });
+});
---
+++ packages/api/src/models/daily-stock-price.model.ts
@@ -0,0 +1,34 @@
+import { PrismaClient, Stock } from "@prisma/client";
+import { FmpApi } from "../lib/fmp-api";
+import prisma from "../prisma/client";
+import { filterNonNullable } from "../utils";
+
+export class DailyStockPriceModel {
+  readonly prisma: PrismaClient;
+  readonly fmpApi: FmpApi;
+
+  constructor(props?: Partial<DailyStockPriceModel>) {
+    this.prisma = props?.prisma || prisma;
+    this.fmpApi = props?.fmpApi || new FmpApi();
+  }
+
+  async findOrCreateAll({ stocks }: { stocks: Stock[] }) {
+    const storedPrices = await this.prisma.dailyStockPrice.findMany({
+      where: {
+        stockId: {
+          in: stocks.map((stock) => stock.id),
+        },
+      },
+    });
+    const storedStockIds = storedPrices.map((price) => price.stockId);
+    const nonExistingStocks = filterNonNullable(stocks.filter((stock) => !storedStockIds.includes(stock.id)));
+    await this.prisma.dailyStockPrice.createMany({
+      data: nonExistingStocks.map((stock) => ({
+        stockId: stock.id,
+        date: new Date(2022, 11, 23),
+        close: 11.23,
+      })),
+    });
+    return [];
+  }
+}
