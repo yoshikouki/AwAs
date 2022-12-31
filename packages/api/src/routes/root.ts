d7a57430303823f076aaf6de9673e0aba9c7a9c8
yoshikouki <yoshikouki@gmail.com>
2022-12-31 11:00:25 +0900

    Add a endpoint to fetch stock prices

--- packages/api/src/routes/root.ts
+++ packages/api/src/routes/root.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import healthRouter from "./health";
 import assetsRouter from "./v1/assets";
+import pricesRouter from "./v1/prices";
 import profileRouter from "./v1/profile";
 import settingsRouter from "./v1/settings";
 
 const rootRouter = Router();
 
-rootRouter.use("/v1", assetsRouter, settingsRouter, profileRouter);
+rootRouter.use("/v1", assetsRouter, settingsRouter, profileRouter, pricesRouter);
 rootRouter.use("/", healthRouter);
 
 export default rootRouter;
---
+++ packages/api/src/services/prices.service.ts
@@ -0,0 +1,25 @@
+import { DailyStockPriceModel } from "../models/daily-stock-price.model";
+import { StockModel } from "../models/stock.model";
+
+export class PricesService {
+  readonly stockModel: StockModel;
+  readonly dailyStockPriceModel: DailyStockPriceModel;
+
+  constructor(props?: Partial<PricesService>) {
+    this.stockModel = props?.stockModel || new StockModel();
+    this.dailyStockPriceModel = props?.dailyStockPriceModel || new DailyStockPriceModel();
+  }
+
+  async getAllBySymbols(symbols: string[]) {
+    const stocks = await this.stockModel.findAllBySymbolsWithLatestDailyPrice(symbols);
+    const result = stocks.map((stock) => {
+      const currentDailyStockPrice = stock.dailyStockPrices[0]
+      return {
+        symbol: stock.symbol,
+        date: currentDailyStockPrice.date,
+        close: currentDailyStockPrice.close,
+      };
+    });
+    return result;
+  }
+}
