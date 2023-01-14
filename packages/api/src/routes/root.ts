996cab815687e2e869fe7641a65f5713f917415d
yoshikouki <yoshikouki@gmail.com>
2023-01-14 21:03:14 +0900

    Delete a old profile endpoint

--- packages/api/src/routes/root.ts
+++ packages/api/src/routes/root.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import healthRouter from "./health";
 import assetsRouter from "./v1/assets";
 import pricesRouter from "./v1/prices";
-import profileRouter from "./v1/profile";
 import settingsRouter from "./v1/settings";
 
 const rootRouter = Router();
 
-rootRouter.use("/v1", assetsRouter, settingsRouter, profileRouter, pricesRouter);
+rootRouter.use("/v1", assetsRouter, settingsRouter, pricesRouter);
 rootRouter.use("/", healthRouter);
 
 export default rootRouter;
