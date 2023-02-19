92e94fbad41ce44c29fff8179f4af858c7f614b3
yoshikouki <yoshikouki@gmail.com>
2023-02-19 22:22:54 +0900

    Increased usability without destructive changes  to arguments

--- src/components/ProfitOrLossText.tsx
+++ src/components/ProfitOrLossText.tsx
@@ -1,15 +1,23 @@
 interface Props {
-  text: string | number;
-  value?: number;
+  text?: string | number;
+  value: number;
   referenceValue: number;
+  digits?: number;
 }
 
-export const ProfitOrLossText = ({ text, value, referenceValue }: Props) => {
-  if (!value) value = typeof text == "number" ? text : parseInt(text);
+export const ProfitOrLossText = ({ text, value, referenceValue, digits = 2 }: Props) => {
+  let displayText: string;
+  if (text) {
+    displayText = text.toString();
+  } else if (Number.isInteger(value)) {
+    displayText = value.toString();
+  } else {
+    displayText = value.toFixed(digits);
+  }
   const color =
     value < referenceValue
       ? "text-red-400 dark:text-red-400"
       : "text-green-600 dark:text-green-400";
 
-  return <span className={`font-bold ${color}`}>{text}</span>;
+  return <span className={`font-bold ${color}`}>{displayText}</span>;
 };
