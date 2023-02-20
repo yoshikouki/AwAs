9eb4d880792a8d60fc4b9630db512d20851b3f96
yoshikouki <yoshikouki@gmail.com>
2023-02-21 08:32:06 +0900

    To use fetched data in other components

--- src/components/Assets.tsx
+++ src/components/Assets.tsx
@@ -1,18 +1,26 @@
 import Link from "next/link";
 import { FaPen } from "react-icons/fa";
+import useSWR from "swr";
+import { apiClient, RouterOutputs } from "../utils/api";
 import AssetsList from "./AssetsList";
 
 const Assets = () => {
+  const { data: assets } = useSWR("/assets", () => apiClient.assets.query());
+  const symbols = assets ? assets.map((asset) => asset.symbol) : [];
+  const { data: prices } = useSWR(
+    symbols ? `/latestPrices/${symbols.join}` : null,
+    () => apiClient.getLatestPrices.query({ symbols })
+  );
   return (
     <div className="prose w-full max-w-4xl">
       <h1 className="px-4">Assets</h1>
 
-      <AssetsList />
+      <AssetsList assets={assets} prices={prices} />
 
       <Link
         href="/assets/edit"
         prefetch={false}
-        className="btn btn-circle btn-lg btn-primary fixed right-0 bottom-0 z-50 mb-20 mr-4 ml-4"
+        className="btn-primary btn-circle btn-lg btn fixed right-0 bottom-0 z-50 mb-20 mr-4 ml-4"
       >
         <FaPen className="text-2xl" />
       </Link>
--- src/components/AssetsList.tsx
+++ src/components/AssetsList.tsx
@@ -1,49 +1,51 @@
+import { RouterOutputs } from "../utils/api";
 import AssetsListItem from "./AssetsListItem";
-import { apiClient } from "../utils/api";
-import useSWR from "swr";
 
-const AssetsList = () => {
-  const { data: assets } = useSWR("/assets", () => apiClient.assets.query());
-  const symbols = assets ? assets.map((asset) => asset.symbol) : [];
-  const { data: prices } = useSWR(
-    assets ? `/latestPrices/${symbols.join}` : null,
-    () => apiClient.getLatestPrices.query({ symbols })
-  );
+interface Props {
+  assets: RouterOutputs["assets"] | undefined;
+  prices: RouterOutputs["getLatestPrices"] | undefined;
+}
+
+const AssetsList = ({ assets, prices }: Props) => {
   return (
-    <div className="overflow-x-auto sm:px-4">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>銘柄</th>
-            <th className="text-end">保有数量</th>
-            <th className="text-end">
-              <div>現在価格</div>
-              <div>取得価格</div>
-            </th>
-            <th className="text-end">
-              <div>現在評価額</div>
-              <div>取得評価額</div>
-            </th>
-            <th className="text-end">
-              <div>利益率</div>
-              <div>利益</div>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {assets?.map((asset) => {
-            const latestDailyPrice = prices && prices[asset.symbol];
-            return (
-              <AssetsListItem
-                asset={asset}
-                latestDailyPrice={latestDailyPrice}
-                key={asset.id}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <h2 className="px-4">All</h2>
+
+      <div className="overflow-x-auto sm:px-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>銘柄</th>
+              <th className="text-end">保有数量</th>
+              <th className="text-end">
+                <div>現在価格</div>
+                <div>取得価格</div>
+              </th>
+              <th className="text-end">
+                <div>現在評価額</div>
+                <div>取得評価額</div>
+              </th>
+              <th className="text-end">
+                <div>利益率</div>
+                <div>利益</div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {assets?.map((asset) => {
+              const latestDailyPrice = prices && prices[asset.symbol];
+              return (
+                <AssetsListItem
+                  asset={asset}
+                  latestDailyPrice={latestDailyPrice}
+                  key={asset.id}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
