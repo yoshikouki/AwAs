a0b089b6c61128a44fb53d3e3e194d38a5c06684
yoshikouki <yoshikouki@gmail.com>
2023-02-21 09:46:03 +0900

    Return 0 to clear duplicate judgments if no argument is found

--- src/utils/calculation.ts
+++ src/utils/calculation.ts
@@ -1,21 +1,45 @@
 import BigNumber from "bignumber.js";
 
-export const sum = (integers: number[]): number => {
-  return integers.reduce((acc, int) => add(acc, int));
-};
-
-export const add = (dirtyNumber: number, int: number): number => {
+export const add = (
+  dirtyNumber: number | undefined,
+  int: number | undefined
+): number => {
+  if (dirtyNumber == undefined || int == undefined) {
+    return 0;
+  }
   return BigNumber(dirtyNumber).plus(int).toNumber();
 };
 
-export const sub = (dirtyNumber: number, int: number): number => {
+export const sub = (
+  dirtyNumber: number | undefined,
+  int: number | undefined
+): number => {
+  if (dirtyNumber == undefined || int == undefined) {
+    return 0;
+  }
   return BigNumber(dirtyNumber).minus(int).toNumber();
 };
 
-export const div = (dirtyNumber: number, int: number): number => {
-  return BigNumber(dirtyNumber).div(int).toNumber();
+export const div = (
+  numerator: number | undefined,
+  denominator: number | undefined
+): number => {
+  if (numerator == undefined || denominator == undefined || denominator === 0) {
+    return 0;
+  }
+  return BigNumber(numerator).div(denominator).toNumber();
 };
 
-export const mul = (dirtyNumber: number, int: number): number => {
+export const mul = (
+  dirtyNumber: number | undefined,
+  int: number | undefined
+): number => {
+  if (dirtyNumber == undefined || int == undefined) {
+    return 0;
+  }
   return BigNumber(dirtyNumber).multipliedBy(int).toNumber();
+};
+
+export const sum = (integers: number[]): number => {
+  return integers.reduce((acc, int) => add(acc, int));
 };
