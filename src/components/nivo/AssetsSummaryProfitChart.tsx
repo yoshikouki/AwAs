6dba44ab3e9f0086c3f10cf18a3a9119c1ad9efa
yoshikouki <yoshikouki@gmail.com>
2023-05-07 14:59:16 +0900

    Fix assets profit bar chart

--- src/components/nivo/AssetsSummaryProfitChart.tsx
+++ src/components/nivo/AssetsSummaryProfitChart.tsx
@@ -30,29 +30,41 @@ export default function AssetsSummaryProfitChart({
     <>
       <div className="h-full w-full space-y-1">
         <div className="flex space-x-1">
-          <div className="grow bg-primary-focus py-8 rounded-lg hover:ring-4 text-4xl">
-            <span className="px-8">{Math.round(data.acquisition.value).toLocaleString("en-US")}</span>
+          <div className="grow rounded-lg bg-primary-focus py-8 text-4xl hover:ring-4">
+            <span className="px-8">
+              {Math.round(data.acquisition.value).toLocaleString("en-US")}
+            </span>
           </div>
           {data.acquisition.pl > 0 && (
             <div
-              className="flex-none bg-success py-8 rounded-lg hover:ring-4 text-4xl"
-              style={{ width: `${data.acquisition.plParentage}%` }}
+              className="flex-none rounded-lg bg-success py-8 text-4xl hover:ring-4"
+              style={{
+                width: `${
+                  100 - (100 / (100 + data.acquisition.plParentage)) * 100
+                }%`,
+              }}
             >
-              <span className="px-8">{Math.round(data.acquisition.pl).toLocaleString("en-US")}</span>
+              <span className="px-8">
+                {Math.round(data.acquisition.pl).toLocaleString("en-US")}
+              </span>
             </div>
           )}
         </div>
 
         <div className="flex space-x-1">
-          <div className="grow bg-primary py-8 rounded-lg hover:ring-4 text-4xl">
-            <span className="px-8">{Math.round(data.market.value).toLocaleString("en-US")}</span>
+          <div className="grow rounded-lg bg-primary py-8 text-4xl hover:ring-4">
+            <span className="px-8">
+              {Math.round(data.market.value).toLocaleString("en-US")}
+            </span>
           </div>
           {data.market.pl > 0 && (
             <div
-              className="flex-none bg-success py-8 rounded-lg hover:ring-4 text-4xl"
+              className="flex-none rounded-lg bg-success py-8 text-4xl hover:ring-4"
               style={{ width: `${data.market.plParentage}%` }}
             >
-              <span className="px-8">{Math.round(data.market.pl).toLocaleString("en-US")}</span>
+              <span className="px-8">
+                {Math.round(data.market.pl).toLocaleString("en-US")}
+              </span>
             </div>
           )}
         </div>
