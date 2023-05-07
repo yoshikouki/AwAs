7f64df2b09c5498c1794694f3da3b7f42e042f24
yoshikouki <yoshikouki@gmail.com>
2023-05-07 10:59:05 +0900

    Styling Assets page

--- src/components/AssetsSummary.tsx
+++ src/components/AssetsSummary.tsx
@@ -1,33 +1,62 @@
 import { percent, sub, sum } from "../utils/calculation";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
+import AssetsSummaryProfitChart from "./nivo/AssetsSummaryProfitChart";
 import { AssetsWithValuations } from "./Assets";
+import classNames from "classnames";
 
 interface Props {
   assetsWithValuations: AssetsWithValuations;
 }
 
-const AssetsSummary = ({
-  assetsWithValuations: assetsWithLatestPrices,
-}: Props) => {
-  const totalAcquisitionValue = assetsWithLatestPrices
-    ? sum(assetsWithLatestPrices.map((asset) => asset.acquisitionValue))
+const AssetsSummary = ({ assetsWithValuations }: Props) => {
+  const totalAcquisitionValue = assetsWithValuations
+    ? sum(assetsWithValuations.map((asset) => asset.acquisitionValue))
     : 0;
-  const portfolioValue = assetsWithLatestPrices
-    ? sum(assetsWithLatestPrices.map((asset) => asset.marketValue))
+  const portfolioValue = assetsWithValuations
+    ? sum(assetsWithValuations.map((asset) => asset.marketValue))
     : 0;
   const totalPL = sub(portfolioValue, totalAcquisitionValue);
   const totalPLPercentage = percent(totalPL, totalAcquisitionValue);
   const numberOfAssetsWithoutValuations =
-    assetsWithLatestPrices?.filter((asset) => asset.averageTradedPrice === 0) || [];
+    assetsWithValuations?.filter((asset) => asset.averageTradedPrice === 0) ||
+    [];
 
   return (
     <div className="prose w-full max-w-4xl">
-      <h2 className="px-4">Summary</h2>
-      取得合計額: {totalAcquisitionValue}
-      評価合計額: {portfolioValue}
-      利益: {totalPL}
-      利益率: {totalPLPercentage}
-      利益計上されていない資産: {numberOfAssetsWithoutValuations.length} 個
+      <div className="mb-8 text-8xl font-extrabold">
+        <span
+          className={classNames("inline-block text-6xl", {
+            "text-success": totalPL > 0,
+            "text-error": totalPL < 0,
+          })}
+        >
+          {totalPL > 0 ? (
+            <FaPlus />
+            ) : (
+            <FaMinus />
+          )}
+        </span>
+        {Math.abs(Math.round(totalPLPercentage * 10) / 10)}
+        <span className="text-4xl ">%</span>
+      </div>
+
+      <div className="mb-4">
+        <AssetsSummaryProfitChart
+          totalAcquisitionValue={totalAcquisitionValue}
+          portfolioValue={portfolioValue}
+          totalPL={totalPL}
+          totalPLPercentage={totalPLPercentage}
+        />
+      </div>
+
+      <div className="text-right text-sm">
+        (利益計上されていない資産:{" "}
+        <span className="font-normal">
+          {numberOfAssetsWithoutValuations.length}
+        </span>{" "}
+        個)
+      </div>
     </div>
   );
 };
